ddb88eba9636e5c80dc467e365ead5c712f89c76
lxr-astro <121371309+lxr-astro@users.noreply.github.com>
2025-03-04 13:55:36 +0900

    Update wezterm.lua

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -8,27 +8,28 @@ local config = wezterm.config_builder()
 config.automatically_reload_config = false
 
 
--- 25-02-14 ?????????tmux
-config.default_prog = {"/opt/homebrew/bin/tmux"} -- mini, tmux in brew
--- config.default_prog = {"/usr/local/bin/tmux"} -- old imac, tmux in /usr
+-- 25-02-14 brew install tmux
+-- config.default_prog = {"/opt/homebrew/bin/tmux"} -- mini, tmux in brew
+config.default_prog = {"/usr/local/bin/tmux"} -- old imac, air, tmux in /usr
 
--- ???????????????
+-- background and color
 config.hide_tab_bar_if_only_one_tab = true
 config.window_decorations = "RESIZE"
--- ????????????
+
 config.window_background_opacity = 0.8
 config.macos_window_background_blur = 20
--- 修改背景颜色
+
 config.colors = {
-  background = "#000a1c", -- 深蓝色背景
+  background = "#000a1c", -- deepblue
 }
 
 
 -- 25-02-07 window size
 --config.font = wezterm.font("JetBrains Mono")
-config.font_size = 14.0
-config.initial_cols = 100 -- ?????????????????????
-config.initial_rows = 30 -- ?????????????????????
+-- config.font_size = 14.0 -- mini
+config.font_size = 18.0 -- air
+config.initial_cols = 100 
+config.initial_rows = 30 
 -- changing the color scheme:
 config.color_scheme = "AdventureTime"
 
